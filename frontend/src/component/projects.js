28be2ec1b9638c36fca4fa2588bdc6afc39773b6
carlconrad <carlconrad98071234@gmail.com>
2024-01-23 22:59:03 +0800

    minor changes

--- frontend/src/component/projects.js
+++ frontend/src/component/projects.js
@@ -1,14 +1,68 @@
 
 import Sidebar  from "./sidebar";
-
+ 
+import '../styles/project.css'
 
 
 function Projects() {
     return (
-      <div className="projects">
+      <div className="projects-sidebar">        
+          
+          
+             <Sidebar  />
 
-        <Sidebar/>
-        </div>
+             
+      
+            <div className="project-content">
+                   
+                   <div className="header">
+                        <div className="project-search">
+                                <input type="text" placeholder="search"></input>
+                        </div>
+
+
+                        <div className="project-profile">
+                               
+                        </div>
+                   </div>
+
+                   <div className="users-project">                      
+                        <div className="create">
+                                    <h2>+</h2>
+                        </div>
+
+
+                        <div className="project">
+
+                           <div className="img">
+                               <img src="./photos/nft.jpg" alt="picture"></img>
+                           </div>
+
+                           <div className="prjTitle">
+                                    <h2>TItle</h2>
+                           </div>
+
+                           <div className="description">
+
+                           </div>
+
+                           <div className="category"> 
+
+                           </div>
+
+                           <div className="people"> 
+
+                           </div>
+
+                           
+
+                        </div>
+                   </div>
+
+             </div>
+
+             
+      </div>
     )
 
     }
--- frontend/src/component/task.js
+++ frontend/src/component/task.js
@@ -9,7 +9,6 @@ function Task() {
              <Sidebar/>            
         </div>
     )
-
     }
 
 
