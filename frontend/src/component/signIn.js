4e764e3321e85f015c32b8f01be3b2ac2d79e405
carlconrad <carlconrad98071234@gmail.com>
2024-02-03 23:29:26 +0800

    users can see only there created products

--- frontend/src/component/signIn.js
+++ frontend/src/component/signIn.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import {  useState } from "react";
-import { Link ,useNavigate} from "react-router-dom";
+import {useState } from "react";
+import {Link ,useNavigate} from "react-router-dom";
 
 
 
@@ -27,30 +27,27 @@ function SignIn() {
             })
             .catch(err => console.log(err));
     }
-    
 
-     
+    return(
+        <div className="Sign-in">    
+                <form onSubmit={handleSignIn}>
 
-    return (
-      <div className="Sign-in">    
-              <form onSubmit={handleSignIn}>
+                    <label htmlFor="username">Username: </label>
+                    <input type="text" placeholder="name" name="username"  required 
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    />
 
-                <label htmlFor="username">Username: </label>
-                <input type="text" placeholder="name" name="username"  required 
-                value={name}
-                 onChange={e => setName(e.target.value)}
-                />
-
-                <label htmlFor="password">Password: </label>
-                <input type="password" placeholder="password" name="password" required
-                value={password}
-                 onChange={e => setPassword(e.target.value)}
-                />
-                
-                <button>SignIn</button>
-              </form>   
-              <Link to="/login"><p>Already have an account?</p></Link>     
-        </div>
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" placeholder="password" name="password" required
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    />
+                    
+                    <button>SignIn</button>
+                </form>   
+                <Link to="/login"><p>Already have an account?</p></Link>     
+       </div>
     )
     }
 
